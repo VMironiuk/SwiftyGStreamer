c778e2dc45ae811eba041673ea0b2925c8cfbdcf
Vladimir Mironiuk <vladimironiuk@gmail.com>
2021-03-06 15:22:32 +0200

    Add starting project

---
+++ SwiftyGStreamer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyGStreamer
+//
+//  Created by Vladimir Mironiuk on 06.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
